5d3ef0aef116c785f713ace93a457faf5e357687
Philip <philiptap2005@outlook.com>
2025-03-04 18:12:59 +0100

    Visual simulation now has BiologicalSimulation.csproj as a dependency.

--- Simple graphical implementation/VisualSimulation.cs
+++ Simple graphical implementation/VisualSimulation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using BioSim.Simulation;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
