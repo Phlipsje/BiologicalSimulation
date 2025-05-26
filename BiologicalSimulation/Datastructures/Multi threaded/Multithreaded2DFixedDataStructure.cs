a454938c5c190a4f945d847e5a052c2f13b93b69
Philip <philiptap2005@outlook.com>
2025-05-26 14:19:17 +0200

    Starting on multithreading

---
+++ BiologicalSimulation/Datastructures/Multi threaded/Multithreaded2DFixedDataStructure.cs
@@ -0,0 +1,10 @@
+using System.Numerics;
+
+namespace BioSim.Datastructures;
+
+//TODO currently just copy pasted, still need to actually implement
+//TODO write explanation here
+public class Multithreaded2DFixedDataStructure : MultithreadedDataStructure
+{
+    
+}
---
+++ BiologicalSimulation/Datastructures/Multi threaded/MultithreadedDataStructure.cs
@@ -0,0 +1,6 @@
+namespace BioSim.Datastructures;
+
+public class MultithreadedDataStructure
+{
+    //TODO throw warning if more threads assigned then chunks that can be run simultaniously
+}
